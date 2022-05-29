e6b138e5550432237fa65838e22d5e63463a49fb
“xcaliber” <baycentral3303@gmail.com>
2022-05-29 15:09:46 +0200

    constructor heeft parameter fout

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         int[] numeric= {1,2,3,4,5,6,7,8,9,0};
         String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht" , "negen", "nul"};
 
-        Translator translate = new Translator();
+        Translator translate = new Translator(int [], String[]);
     }
 }
