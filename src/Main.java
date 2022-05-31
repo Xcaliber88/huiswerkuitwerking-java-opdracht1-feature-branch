df6079b50b7b5529c34a37d83f2590af74ed1314
“xcaliber” <baycentral3303@gmail.com>
2022-05-31 19:49:46 +0200

    uitwerkingen verbeterd

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Integer[] numeric= {1,2,3,4,5,6,7,8,9,0};
-        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht" , "negen", "nul"};
-        Boolean play=true;
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+        Boolean play = true;
         String ongeldig = "ongeldige invoer";
 
 
         Scanner userInputScanner = new Scanner(System.in);
 
-        String input = userInputScanner.nextLine();
 
-        Translator translator = new Translator(numeric,alphabetic);
+        Translator translator = new Translator(numeric, alphabetic);
 
-        while(play){
+        while (play) {
             System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
-            if(input.equalsIgnoreCase("x")){
-                play= false;
-            }else if (input.equalsIgnoreCase("v")){
+            String input = userInputScanner.nextLine();
+            if (input.equalsIgnoreCase("x")) {
+                play = false;
+            } else if (input.equalsIgnoreCase("v")) {
                 System.out.println("Type een cijfer in van 0 t/m 9");
-            }else{
-                System.out.println(ongeldig);
-            }
-
-            int number= userInputScanner.nextInt();
-
-            if(number<10) {
-                String result= translator.translate(number);
-                System.out.println("De vertaling van " + number + " is " + result);
-            }else {
-                System.out.println("ongeldig");
+                Integer number = userInputScanner.nextInt();
+                userInputScanner.nextLine();
+                if (number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println("ongeldige invoer");
+                }
+            } else {
+                System.out.println("ongeldige invoer");
             }
 
         }
--- src/Translator.java
+++ src/Translator.java
@@ -3,15 +3,16 @@ import java.util.Map;
 
 public class Translator {
 
-    HashMap<Integer,String> numericAlpha;
+    Map<Integer, String> numericAlpha = new HashMap<>();
 
-    public Translator (Integer[] numeric,String[] alphabetic){
-        for(int i =0; i <numeric.length; i++){
-            numericAlpha.put(i, alphabetic[i]);
+    public Translator(Integer[] numeric, String[] alphabetic) {
+        for (int i = 0; i < numeric.length; i++) {
+            numericAlpha.put(numeric[i], alphabetic[i]);
         }
     }
 
-    public String translate (Integer number) {
+    public String translate(Integer number) {
+
         return numericAlpha.get(number);
     }
 
